1f06cbfdac490f56ac3c7389f6675eca9701c7fb
Tayhlow8 <tayna.schultz@gmail.com>
2026-05-11 19:30:34 -0300

    seo: adicionar sitemap XML e schema markup

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import viteImagemin from "vite-plugin-imagemin";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
@@ -12,6 +13,10 @@ export default defineConfig({
       mozjpeg: { quality: 75 },
       pngquant: { quality: [0.7, 0.9], speed: 4 },
       svgo: { plugins: [{ removeViewBox: false }] },
+    }),
+    sitemap({
+      hostname: "https://www.caixadaguarevestimentos.com.br",
+      dynamicRoutes: ["/", "/impermeabilizacao"],
     }),
   ],
   resolve: {
